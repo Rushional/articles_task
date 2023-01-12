dc4852c3c5b640b79057c33f7aa388eeb2ec646f
Rushional <Rushional2@gmail.com>
2023-01-12 13:31:54 +0400

    first commit

---
+++ src/main/java/com/rushional/articles_task/ArticlesTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.articles_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushional/articles_task/ArticlesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.articles_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
